86b1989054b0c029d44ae9e81f06b315a00c7978
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-21 03:46:00 +0300

    Create hot_drink.java

---
+++ hot_drink.java
@@ -0,0 +1,17 @@
+public class ГорячийНапиток {
+    private String name;
+    private int volume;
+
+    public ГорячийНапиток(String name, int volume) {
+        this.name = name;
+        this.volume = volume;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+}
