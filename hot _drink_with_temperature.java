d60939b59a23d18893a1504ae11fb142537cc213
Natalya <159589443+Natalya-Prokofeva@users.noreply.github.com>
2024-09-21 04:13:06 +0300

    Update hot _drink_with_temperature.java

--- hot _drink_with_temperature.java
+++ hot _drink_with_temperature.java
@@ -1,12 +1,22 @@
-public class ГорячийНапитокСТемпературой extends ГорячийНапиток {
+// Наследник класса ГорячийНапиток с полем температура
+class СпецифическийГорячийНапиток extends ГорячийНапиток {
     private int температура;
 
-    public ГорячийНапитокСТемпературой(String name, int volume, int температура) {
+    public СпецифическийГорячийНапиток(String name, int volume, int температура) {
         super(name, volume);
         this.температура = температура;
     }
 
     public int getТемпература() {
         return температура;
+    }
+
+    @Override
+    public String toString() {
+        return "СпецифическийГорячийНапиток{" +
+                "name='" + getName() + '\'' +
+                ", volume=" + getVolume() +
+                ", температура=" + температура +
+                '}';
     }
 }
